7d5f53bc5d3a7dc1fc5ae92b84d0b8d5837d7272
mofner <mofner@student.tgm.ac.at>
2024-10-20 22:44:26 +0200

    UML überarbeitet

---
+++ src/main/java/WordTrainer.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+/**
+ * Klasse zur Verwaltung des Worttrainers
+ */
+public class WordTrainer {
+    private List<WordImagePair> wordImagePairs;
+    private WordImagePair selectedPair;
+    private int totalAttempts;
+    private int correctAttempts;
+
+    // Konstruktor und grundlegende Methoden kommen hierhin
+}
