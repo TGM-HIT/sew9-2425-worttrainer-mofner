5cf345a32f0f70898b75fb76882623f872f3b6bf
mofner <mofner@student.tgm.ac.at>
2024-10-25 11:11:21 +0200

    load-Methode gefixt

--- src/main/java/FilePersistence.java
+++ src/main/java/FilePersistence.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class FilePersistence {
 
+
     // Methode zum Speichern des WordTrainer-Zustands
     public static void save(WordTrainer trainer, String fileName) throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
@@ -39,7 +40,16 @@ public class FilePersistence {
                 }
             }
         }
-        WordTrainer trainer = new WordTrainer(pairs);
+        WordTrainer trainer;
+        if(pairs!=null && !pairs.isEmpty()) {
+            trainer = new WordTrainer(pairs);
+        } else {
+            List<WordImagePair> wordImagePairs = new ArrayList<>();
+            wordImagePairs.add(new WordImagePair("Hund", "https://img.freepik.com/vektoren-premium/hund-mit-hand-gezeichnet_885110-3.jpg"));
+            wordImagePairs.add(new WordImagePair("Ball", "https://media.istockphoto.com/id/91712739/de/foto/fu%C3%9Fball-ball.jpg?s=612x612&w=0&k=20&c=kzXem71bcuY3AjXO5Vac9szUALvLBmgIZ3feCrDuR2c="));
+            wordImagePairs.add(new WordImagePair("Apfel", "https://cdn.pixabay.com/photo/2016/10/30/18/01/apple-1783882_640.png"));
+            trainer = new WordTrainer(wordImagePairs);
+        }
         trainer.setCorrectAttempts(correctAttempts);
         trainer.setTotalAttempts(totalAttempts);
         return trainer;
